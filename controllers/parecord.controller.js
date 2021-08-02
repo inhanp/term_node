584ea8ded09a6597c902f40712a5e1c30fbea76c
Inhan Park <inhan.park.95@gmail.com>
2021-08-03 00:23:02 +0900

    Initial commit

---
+++ controllers/parecord.controller.js
@@ -0,0 +1,25 @@
+const parecordService = require('../services/parecord.service')
+
+module.exports = {
+    createPArecord,
+    getPArecords,
+    deletePArecord
+};
+
+
+function createPArecord(req, res, next) {
+  //TODO: via parecordSerice you should add a PA record and respond to the client confirming that the record was successfully added.
+
+
+}
+
+function getPArecords(req,res,next){
+//TODO: return all parecords from the database and send to the client.
+    }
+
+
+function deletePArecord(req,res,next){
+
+//TODO: delete parecord from the database and respond to the client by conforming the action.
+
+}
---
+++ services/parecord.service.js
@@ -0,0 +1,38 @@
+const db = require('../_helpers/database');
+const PArecord = db.PArecord;
+
+
+module.exports = {
+    //getAllPArecords,
+   // addPArecord,
+   // deletePArecord
+}
+
+
+//TODO: write the necessary functions that will address the needs of parecord.controller. Hint: look at the signatures in the module.exports. Hint2: look at user.service to see how you can interact with the database. Hint3: look at the class material.
+
+
+
+
+async function addPArecord(parecord, username) {
+
+
+    // validate
+    if (await PArecord.findOne({ createdBy: username, createdDate: parecord.createdDate  })) {
+        throw 'Parecord created by"' + parecord.createdBy +" on "+ parecord.createdDate +'" already exists';
+    }
+    else if(!username){
+        throw 'Error with the user submitting the request. User information missing. Malformed request.';
+    }
+    //populate missing fields in the parecord object
+    let newrecord= parecord;
+    parecord.createdBy = username;
+    parecord.createdDate =  Date.now();
+
+    dbrecord = new PArecord(newrecord);
+
+
+    // save the record
+    await dbrecord.save();
+
+}
