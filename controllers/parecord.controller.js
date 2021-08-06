322e0e378d0be6e47d636b1bc1946260ee8ee4d4
Inhan Park <inhan.park.95@gmail.com>
2021-08-06 13:16:52 +0900

    user final

--- controllers/parecord.controller.js
+++ controllers/parecord.controller.js
@@ -8,21 +8,20 @@ module.exports = {
 
 
 function createPArecord(req, res, next) {
-    parecordService.addPArecord(req.body, ).then().catch(err => next(err))
-  //TODO: via parecordSerice you should add a PA record and respond to the client confirming that the record was successfully added.
+    parecordService.addPArecord(req.body, req.user.sub).then(paracord => res.json(paracord)).catch(err => next(err))
+  // via parecordSerice you should add a PA record and respond to the client confirming that the record was successfully added.
 
 
 }
 
 function getPArecords(req,res,next){
-    console.log(req.user.sub);
-//TODO: return all parecords from the database and send to the client.
+// return all parecords from the database and send to the client.
     parecordService.getAllPArecords().then(paracords => res.json(paracords)).catch(err => next(err))
 }
 
 
 function deletePArecord(req,res,next){
-    console.log(req.param.date);
-//TODO: delete parecord from the database and respond to the client by conforming the action.
+    parecordService.deletePArecord(req.params.date).then(parecord => res.json(parecord)).catch(err => next(err))
+// delete parecord from the database and respond to the client by conforming the action.
 
 }
--- services/parecord.service.js
+++ services/parecord.service.js
@@ -9,7 +9,7 @@ module.exports = {
 }
 
 
-//TODO: write the necessary functions that will address the needs of parecord.controller. Hint: look at the signatures in the module.exports. Hint2: look at user.service to see how you can interact with the database. Hint3: look at the class material.
+// write the necessary functions that will address the needs of parecord.controller. Hint: look at the signatures in the module.exports. Hint2: look at user.service to see how you can interact with the database. Hint3: look at the class material.
 
 async function getAllPArecords() {
     return await PArecord.find().select('-hash');
@@ -39,4 +39,6 @@ async function addPArecord(parecord, username) {
 
 }
 
-async function deletePArecord() {}
+async function deletePArecord(date) {
+    await PArecord.findOne({ createdDate: date }).remove()
+}
